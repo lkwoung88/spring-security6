97f06808f24d3cdff414cf55bb00318a7344917c
lkwoung88 <lkwoung88@gmail.com>
2024-05-07 03:33:49 +0900

    init : 시큐리티 설정

---
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -0,0 +1,39 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    /**
+     * application.yml과 중복된 설정이 있는 경우에는 아래 Bean이 우선순위를 가진다.
+     * @return
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
