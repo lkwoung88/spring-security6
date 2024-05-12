d2d67e1e772c76016b23f701073a3d911f0fe9fc
lkwoung88 <lkwoung88@gmail.com>
2024-05-12 20:44:09 +0900

    fix : http basic 사용

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,22 +1,15 @@
 package io.security.springsecuritymaster;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -26,22 +19,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/loginPage")
-                        .loginProcessingUrl("/loginProc")
-                        .defaultSuccessUrl("/", false) // alwaysUse의 기본값은 false
-                        .failureUrl("/failed")
-                        .usernameParameter("userId")
-                        .passwordParameter("passwd")
-                        .successHandler(((request, response, authentication) -> {
-                            System.out.println("authentication : " + authentication);
-                            response.sendRedirect("/home");
-                        }))
-                        .failureHandler((request, response, exception) -> {
-                            System.out.println("exception : " + exception.getMessage());
-                            response.sendRedirect("/login");
-                        })
-                        .permitAll());
+                .formLogin(Customizer.withDefaults());
+//                .formLogin(form -> form
+//                        .loginPage("/loginPage")
+//                        .loginProcessingUrl("/loginProc")
+//                        .defaultSuccessUrl("/", false) // alwaysUse의 기본값은 false
+//                        .failureUrl("/failed")
+//                        .usernameParameter("userId")
+//                        .passwordParameter("passwd")
+//                        .successHandler(((request, response, authentication) -> {
+//                            System.out.println("authentication : " + authentication);
+//                            response.sendRedirect("/home");
+//                        }))
+//                        .failureHandler((request, response, exception) -> {
+//                            System.out.println("exception : " + exception.getMessage());
+//                            response.sendRedirect("/login");
+//                        })
+//                        .permitAll());
 
         return http.build();
     }
